539e2222d9aadee44c22ff9a3bdbc0ac4faa132f
nimisha84 <nimisha.shrivastava84@gmail.com>
2019-09-12 13:29:57 -0700

    testing rules in eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -4,6 +4,7 @@ module.exports = {
         "commonjs": true,
         "es6": true
     },
+    "extends": "eslint:recommended",
     "globals": {
         "Atomics": "readonly",
         "SharedArrayBuffer": "readonly"
@@ -12,5 +13,7 @@ module.exports = {
         "ecmaVersion": 2018
     },
     "rules": {
+	"semi":["error", "always"]
+	
     }
 };
